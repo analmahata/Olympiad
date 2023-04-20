5f1734bfb5a555527b1e7c559e14f2d61183d81a
Anal Mahata <developer.anal@gmail.com>
2023-04-20 16:46:48 +0530

    nothing

---
+++ src/scripts/javascripts/main.js
@@ -0,0 +1,23 @@
+const toggleButton = document.querySelector('.theme_change_icon');
+const colors = document.querySelectorAll('.color');
+
+colors.forEach(color => {
+    color.addEventListener('click', (e) => {
+        colors.forEach(c => c.classList.remove('selected'));
+        const theme = color.getAttribute('data-color');
+        document.body.setAttribute('data-theme', theme);
+        color.classList.add('selected');
+    });
+});
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+
+let button = document.querySelector('.burger');
+button.addEventListener('click', function () {
+    button.classList.toggle('active');
+})
+
+// Button animations
